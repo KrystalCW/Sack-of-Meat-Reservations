7b726d8ef8b15c2652a6c024f58444ec4317197c
Ryan Grunest <ryangrunest45@gmail.com>
2019-04-13 11:38:39 -0700

    more code

--- public/app.js
+++ public/app.js
@@ -8,10 +8,33 @@ $('#add-btn').on('click', (event) => {
         uniqueID: $('#uniqueID').val().trim()
     }
     // Question: What does this code do??
-    $.post("/api/tables", tableRes)
-    .then(function(data) {
-      console.log(data);
-      alert("Reservation Added!");
-    });
+    $.get('/api/tables', function(data) {
+        if (data.length <= 4) {
+            $.post("/api/tables", tableRes)
+            .then(function(data) {
+            console.log(data);
+            alert("Reservation Added!");
+            });
+        } else {
+            $.post("/api/waitlist", tableRes)
+            .then(function(data) {
+            console.log(data);
+            alert("Reservations are full. However, you're on the waitlist.");
+            });
+        }
+    })
+})
 
+$('#view-tables').on('click', (event) => {
+    event.preventDefault();
+    $.get('api/tables', (data) => {
+        console.log(data);
+
+        for (var i = 0; i < data.length; i++) {
+            let list = $('<li>').text(data[i]);
+            $('#tableSection').append($(list));
+        }   
+       
+
+    })
 })
